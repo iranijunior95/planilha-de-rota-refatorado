b1f70ead9e892bfdfac4764e986d570615252297
Irani Junior <iranijunior95@gmail.com>
2023-05-25 22:16:46 -0300

    commit tabela individual
    renderização tabela individual

--- js/script_index.js
+++ js/script_index.js
@@ -187,7 +187,7 @@ const renderizarDadosTabela = () => {
         const pNomeFuncionario = criarElementosHtml('p');
         const btnImprimir = criarElementosHtml('button', '', '<i class="fa fa-print" aria-hidden="true"></i> <b>IMPRIMIR</b>');
 
-        divRowCabecalho.classList.add('row');
+        divRowCabecalho.classList.add('row', 'row-cabecalho');
         divColCabecalho.classList.add('col-md-12', 'text-center');
 
         img.src = "./img/logo_oficial_grupo_vicunha.png";
@@ -308,6 +308,8 @@ const criarRowsThead = (estadoHead) => {
         return tr;
 
     }else {
+        thCidades.classList.remove('thCidades');
+        thCidades.classList.add('thCidadesConteudo');
 
         tr.appendChild(thDiaria);
         tr.appendChild(thMotoristaCarregamentos);
@@ -347,6 +349,10 @@ const criarRowsTbody = (dados, estadoBody) => {
 
     btnDeletar.addEventListener('click', () => {
         deletarDados(dados['id']);
+    });
+
+    btnVizualizar.addEventListener('click', () => {
+        imprimirPlanilhaIndividual(dados['id']);
     });
 
     pMotorista.innerText = dados['motorista'];
@@ -567,21 +573,50 @@ const somarValorTotalDiarias = () => {
 
 //Funçoes para imprimir planilha e renderizar os dados a serem impressos
 const imprimirDadosPlanilha = () => {
+    const dadosPlanilhaLocalStorage = JSON.parse(localStorage.getItem('dadosPlanilha')) ?? [];
+
     const head = document.querySelector('head');
+    const conteudoCabecalho = document.getElementsByClassName('row-cabecalho');
+    const thead = criarElementosHtml('thead');
+    const tbody = criarElementosHtml('tbody');
+    const conteudoAssinatura = document.getElementsByClassName('row-assinatura');
+
+    thead.appendChild(criarRowsThead(false));
+
+    dadosPlanilhaLocalStorage.forEach((dados) => {
+        tbody.appendChild(criarRowsTbody(dados, false));
+    });
+    
     const conteudoHtml = `<!DOCTYPE html>
                           <html lang="pt-br">
                             ${head.innerHTML}
-                            <body>
+                            <body style="background-color: #FFFFFF;">
                                 <div class="container-fluid">
-                                    <div class="card">
-                                        
-                                        <div class="card-body" id="body-listar-tabela-imprimir">
-
-
-                    
-                                        </div>
-                    
+                                    <div class="row">
+                                        ${conteudoCabecalho[0].innerHTML}
                                     </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12 table-responsive">
+                                            <table class="table table-bordered table-listar">
+                                                <thead>
+                                                ${thead.innerHTML}
+                                                </thead>
+
+                                                <tbody>
+                                                ${tbody.innerHTML}
+                                                <tr>
+                                                    <td colspan="6"><b>VALOR TOTAL:</b> R$ ${somarValorTotalDiarias()}</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+
+                                    <div class="row row-assinatura">
+                                        ${conteudoAssinatura[0].innerHTML}
+                                    </div>
+                                    
                                 </div>
 
                                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -591,96 +626,174 @@ const imprimirDadosPlanilha = () => {
                             </body>
                           </html>`;
                           
-    
-    
     const win = window.open('', '', 'height=800, width=1200');
 
     win.document.write(conteudoHtml);
 
-    renderizarDadosConteudo();
-    
-    //console.log(conteudoHtml);
+    setTimeout(function(){ 
+        win.print()
+        win.close()
+    }, 1000);
     
 };
 
-const renderizarDadosConteudo = () => {
-    const dadosPlanilhaLocalStorage = JSON.parse(localStorage.getItem('dadosPlanilha')) ?? [];
-    const bodyListarTabelaImprimir = document.getElementById('body-listar-tabela-imprimir');
-    
-    bodyListarTabelaImprimir.innerHTML = '';
+const imprimirPlanilhaIndividual = (id) => {
+    const dadosPlanilha = retornaConteudoVizualizar(id);
+    const divImg = criarElementosHtml('div');
+    const img = criarElementosHtml('img');
+    const divTableMotorista = criarElementosHtml('div');
+    const tableMotorista = criarElementosHtml('table')
 
-    if(dadosPlanilhaLocalStorage.length === 0) {
-        const p = criarElementosHtml('p', 'TABELA VAZIA!!!');
+    img.src = "./img/logo_oficial_grupo_vicunha.png";
+    img.classList.add('img-fluid');
+    img.alt = "Logo Grupo Vicunha";
+    img.width = "200";
+    img.height = "200";
 
-        p.classList.add('text-center');
-        bodyListarTabelaImprimir.appendChild(p);
+    divImg.appendChild(img);
+
+    tableMotorista.classList.add('table', 'table-bordered');
+
+    divTableMotorista.appendChild(tableMotorista);
+
+    const conteudoHtml = `<!DOCTYPE html>
+                          <html lang="pt-br">
+                            <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                <link rel="shortcut icon" href="./img/favicon_vicunha_icon.ico" type="image/x-icon" />
+
+                                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css">
+                                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+                                <style>
+                                    .table td, .table th {
+                                        padding: 5px;
+                                        border: 1px solid #adaaaa;
+                                    }
+                                </style>
+
+                                <title>PLANILHA DE ROTA</title>
+                            </head>
+                            <body>
+                                <div class="container">
+
+                                    <div class="row">
+                                        <div class="col-md-12 text-center">
+                                            ${divImg.innerHTML}                       
+                                        </div>
+                                    </div>
+
+                                    <br>
+
+                                    <div class="row">
+                                        <div class="col-md-2"></div>
+
+                                        <div class="col-md-8">
+                                            ${divTableMotorista.innerHTML}
+                                        </div>
+
+                                        <div class="col-md-2"></div>
+                                    </div>
+
+                                </div>
+
+                                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+                                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                            </body>
+                          </html>`;
+
+    const win = window.open('', '', 'height=800, width=1200');
+
+    win.document.write(conteudoHtml);
+
+    console.log(conteudoHtml);
+};
+
+const retornaConteudoVizualizar = (id) => {
+    let dadosPlanilha = JSON.parse(localStorage.getItem('dadosPlanilha')) ?? [];
+
+    dadosPlanilha = dadosPlanilha.filter(function(value) {
+
+        return value.id == id;
+    });
+
+    let qtdAjudantes = 0;
+
+    let ajudante01 = {
+        nome: '',
+        diaria: '0,00'
+    };
+
+    let ajudante02 = {
+        nome: '',
+        diaria: '0,00'
+    };
+
+    let valorDiarias = '0,00';
+
+    if(dadosPlanilha[0]['ajudantes'].length == 0){
+        qtdAjudantes = 0;
+    }
+
+    if(dadosPlanilha[0]['ajudantes'].length == 1){
+        qtdAjudantes = 1;
+        ajudante01 = {
+            nome: dadosPlanilha[0]['ajudantes'][0],
+            diaria: '0,00'
+        };
+    }
+
+    if(dadosPlanilha[0]['ajudantes'].length == 2){
+        qtdAjudantes = 2;
+        ajudante01 = {
+            nome: dadosPlanilha[0]['ajudantes'][0],
+            diaria: '0,00'
+        };
+        ajudante02 = {
+            nome: dadosPlanilha[0]['ajudantes'][1],
+            diaria: '0,00'
+        };
+    }
+
+    if(dadosPlanilha[0]['diaria'] === '' || dadosPlanilha[0]['diaria'] === '0,00') {
+        valorDiarias = '0,00';
+
     }else {
-        const divRowCabecalho = criarElementosHtml('div');
-        const divColCabecalho = criarElementosHtml('div');
-        const img = criarElementosHtml('img');
-        const br = criarElementosHtml('br');
-        const h5Cabecalho = criarElementosHtml('h5');
-        const divRowConteudo = criarElementosHtml('div');
-        const divColConteudo = criarElementosHtml('div');
-        const table = criarElementosHtml('table');
-        const thead = criarElementosHtml('thead');
-        const tbody = criarElementosHtml('tbody');
-        const trValorTotal = criarElementosHtml('tr', '', `<td colspan="6"><b>VALOR TOTAL:</b> R$ ${somarValorTotalDiarias()}</td>`);
-        const divRowAssinaturaFuncionario = criarElementosHtml('div');
-        const divColAssinaturaFuncionario = criarElementosHtml('div');
-        const pLinha = criarElementosHtml('p');
-        const pNomeFuncionario = criarElementosHtml('p');
+        const dividirPor = qtdAjudantes + 1;
+        const result = (parseInt(dadosPlanilha[0]['diaria'].replace(/[^\d]+/g,'')) / dividirPor) / 100;
 
-        divRowCabecalho.classList.add('row');
-        divColCabecalho.classList.add('col-md-12', 'text-center');
+        valorDiarias = result.toLocaleString('pt-br', {minimumFractionDigits: 2});
 
-        img.src = "./img/logo_oficial_grupo_vicunha.png";
-        img.classList.add('img-fluid');
-        img.alt = "Logo Grupo Vicunha";
-        img.width = "200";
-        img.height = "200";
+        if(qtdAjudantes == 1) {
+            ajudante01 = {
+                nome: dadosPlanilha[0]['ajudantes'][0],
+                diaria: valorDiarias
+            };
+        }
 
-        h5Cabecalho.classList.add('h5-cabecalho');
-        h5Cabecalho.innerHTML = `PLANILHA DE ROTA - ${validaCampoDataPlanilha()}`;
+        if(qtdAjudantes == 2) {
+            ajudante01 = {
+                nome: dadosPlanilha[0]['ajudantes'][0],
+                diaria: valorDiarias
+            };
 
-        divRowConteudo.classList.add('row');
-        divColConteudo.classList.add('col-md-12', 'table-responsive');
-        
-        table.classList.add('table', 'table-bordered', 'table-listar');
-
-        divRowAssinaturaFuncionario.classList.add('row', 'row-assinatura');
-        divColAssinaturaFuncionario.classList.add('col-md-12', 'text-center');
-
-        pNomeFuncionario.classList.add('pNomeFuncionario');
-
-        pLinha.innerText = '__________________________________________________________';
-        pNomeFuncionario.innerText = validaCampoNomeFuncionario();
-
-        divColAssinaturaFuncionario.appendChild(pLinha);
-        divColAssinaturaFuncionario.appendChild(pNomeFuncionario);
-        divRowAssinaturaFuncionario.appendChild(divColAssinaturaFuncionario);
-
-        divColCabecalho.appendChild(img);
-        divColCabecalho.appendChild(br);
-        divColCabecalho.appendChild(h5Cabecalho);
-        divRowCabecalho.appendChild(divColCabecalho);
-        bodyListarTabela.appendChild(divRowCabecalho);
-
-        thead.appendChild(criarRowsThead(false));
-        table.appendChild(thead);
-
-        dadosPlanilhaLocalStorage.forEach((dados) => {
-            tbody.appendChild(criarRowsTbody(dados, false));
-        });
-
-        table.appendChild(tbody);  
-        table.appendChild(trValorTotal);  
-        divColConteudo.appendChild(table);
-        divRowConteudo.appendChild(divColConteudo);
-        bodyListarTabelaImprimir.appendChild(divRowConteudo);
-        bodyListarTabelaImprimir.appendChild(divRowAssinaturaFuncionario);
+            ajudante02 = {
+                nome: dadosPlanilha[0]['ajudantes'][1],
+                diaria: valorDiarias
+            };
+        }
 
     }
+
+    return {
+        dados: dadosPlanilha,
+        ajudante01: ajudante01,
+        ajudante02: ajudante02,
+        valorDiarias: valorDiarias
+    };
 };
 
 document.getElementById('input-diaria').addEventListener('keyup', formatarCampoValor);
