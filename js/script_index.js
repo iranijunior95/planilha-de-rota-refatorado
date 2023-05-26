dd3e4db5344113906cd2cec6e3e75c4dff63e874
Irani Junior <iranijunior95@gmail.com>
2023-05-26 14:13:51 -0300

    commit
    commit test

--- js/script_index.js
+++ js/script_index.js
@@ -874,7 +874,6 @@ const imprimirPlanilhaIndividual = (id) => {
 
                                     <p class="font-weight-bold">Km SAÍDA:</p>
                                     <p class="font-weight-bold">Km CHEGADA:</p>
-
                                 </div>
 
                                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
